236f099cb11d8727225e0c59245552e9f000e36c
Ross Rheingans-Yoo <ross@r-y.io>
2023-01-19 05:46:26 +0000

    change C entrypoint mock from c_main() to c_run()

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+
+const spectrary: bool = false;
+const umbrary:   bool = false;
+
 const main_c = @cImport({
     // See https://github.com/ziglang/zig/issues/515
     @cDefine("_NO_CRT_STDIO_INLINE", "1");
@@ -6,27 +10,13 @@ const main_c = @cImport({
 });
 
 pub fn main() u8 {
-    var c_argv: [2][*c]u8 = undefined;
-    var s0: [*c]u8 = blk: {
-        break :blk &struct {
-            var string_temp: @TypeOf("zig build run".*) = "zig build run".*;
-        }.string_temp;
-    };
-    var s1: [*c]u8 = blk: {
-        break :blk &struct {
-            var string_temp: @TypeOf("500".*) = "500".*;
-        }.string_temp;
-    };
-    c_argv[0] = s0;
-    c_argv[1] = s1;
     return @intCast(u8,
-        main_c.c_main(
-            @as(c_int, 2),
-            @ptrCast([*c][*c]u8,
-                @alignCast(std.meta.alignment([*c][*c]u8),
-                    &c_argv
-                )
-            )
+        main_c.c_run(
+            500,
+            @boolToInt(false),
+            null,
+            @boolToInt(false),
+            null
         )
     );
 }
